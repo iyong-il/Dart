f73c4b96c60d4712586bed93db76dbd790dc9555
iyong-il <lyi1233313@gmail.com>
2023-08-15 01:11:06 +0900

    static 복습

--- 문법/bin/static.dart
+++ 문법/bin/static.dart
@@ -11,6 +11,8 @@ void main() {
   Employee.building = 'G밸리';
   ryujin.printNameAndBuilding();
   Employee.printBuilding();
+
+  print(Car.totalDistance(300));
 }
 
 class Weather {
@@ -32,5 +34,23 @@ class Employee {
 
   static void printBuilding() {
     print('저는 $building 건물에서 근무 중 입니다.');
+  }
+}
+
+class Car {
+  static int distance = 100;
+  // 인스턴스멤버는 스태틱메서드 안에 들어갈 수 없다.
+  String weather = 'hot';
+
+  static int totalDistance(int thisTripDistance) {
+    return distance + thisTripDistance;
+  }
+
+  Car() {}
+
+// factory constructor는 반드시 상위 클래스 아니면, 하위클래스를 리턴해줘야 한다.
+// 이름을 정해주지 않아도 된다.
+  factory Car.white() {
+    return Car();
   }
 }
